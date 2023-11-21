76cf187accfa31796b64a44d06a175d8dae47be2
Quy Nguyen <youngboy5801@gmail.com>
2023-11-21 17:53:09 +0700

    merge with previous implemented xsd scratch

--- JavaVisualizer/src/main/java/org/example/Main.java
+++ JavaVisualizer/src/main/java/org/example/Main.java
@@ -1,40 +1,104 @@
 package org.example;
 
-import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.utils.SourceRoot;
+import net.sourceforge.plantuml.SourceStringReader;
 import org.example.PlantUMLParser.Controller.PlantUMLVisitor;
-import org.example.PlantUMLParser.Model.ClassRelation;
+import org.example.PlantUMLParser.Controller.XsdConverterVisitor;
 import org.example.PlantUMLParser.Model.PlantUMLDiag;
-
+import org.apache.commons.cli.*;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    List<List<String>> test;
     public static void main(String[] args) throws IOException {
-        Path pathToSource = Paths.get("C:\\Users\\xnyuq\\IdeaProjects\\CodeVisualizer\\JavaVisualizer\\src");
-        SourceRoot sourceRoot = new SourceRoot(pathToSource);
-        sourceRoot.tryToParse();
-        List<CompilationUnit> compilationUnits = sourceRoot.getCompilationUnits();
+        Options options = new Options();
+        options.addOption("g", "generate", true, "Generate output (uml or xsd)");
+        options.addOption("o", "output", true, "Output folder path");
+        options.addOption("r", "render", true, "Render format (png or ascii)");
 
-        PlantUMLVisitor visitor = new PlantUMLVisitor();
-        PlantUMLDiag plantUML = new PlantUMLDiag();
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
 
-        // Maintain a list of project's class names for later use
-        for (CompilationUnit cu: compilationUnits) {
-            String className = cu.getPrimaryTypeName().get();
-            plantUML.addClassName(className);
+            String sourcePath = cmd.getOptionValue("i");
+            String generateOption = cmd.getOptionValue("g");
+            String outputFolder = cmd.getOptionValue("o", "output/");
+            String renderFormat = cmd.getOptionValue("r", "png");
+
+            // Accept the path to the source from the user
+
+            if ("uml".equals(generateOption) || "xsd".equals(generateOption)) {
+                Path pathToSource = Paths.get(sourcePath);
+                SourceRoot sourceRoot = new SourceRoot(pathToSource);
+                sourceRoot.tryToParse();
+                List<CompilationUnit> compilationUnits = sourceRoot.getCompilationUnits();
+
+                PlantUMLVisitor umlVisitor = new PlantUMLVisitor();
+                PlantUMLDiag plantUML = new PlantUMLDiag();
+
+                // Maintain a list of project's class names for later use
+                for (CompilationUnit cu : compilationUnits) {
+                    String className = cu.getPrimaryTypeName().get();
+                    plantUML.addClassName(className);
+                }
+
+                for (CompilationUnit cu : compilationUnits) {
+                    umlVisitor.visit(cu, plantUML);
+                }
+
+                if ("uml".equals(generateOption)) {
+                    // Display PlantUML diagrams at runtime
+                    String plantUmlSource = plantUML.toString();
+                    renderPlantUML(plantUmlSource, renderFormat);
+                } else if ("xsd".equals(generateOption)) {
+                    // Generate XSD files
+                    for (CompilationUnit cu : compilationUnits) {
+                        String className = cu.getPrimaryTypeName().get();
+                        XsdConverterVisitor xsdVisitor = new XsdConverterVisitor(outputFolder + className + ".xsd");
+                        System.out.println("Parsing " + className + "...");
+                        xsdVisitor.visit(cu, plantUML.classNames);
+                        xsdVisitor.closeWriter();
+                    }
+                }
+            } else {
+                System.err.println("Invalid option for -g/--generate. Use 'uml' or 'xsd'.");
+            }
+        } catch (ParseException e) {
+            System.err.println("Error parsing command-line arguments: " + e.getMessage());
         }
-        for (CompilationUnit cu: compilationUnits) {
-            visitor.visit(cu, plantUML);
+    }
+
+    private static void renderPlantUML(String plantUmlSource, String renderFormat) {
+        try {
+            if ("png".equals(renderFormat)) {
+                renderPlantUMLAsPNG(plantUmlSource);
+            } else if ("ascii".equals(renderFormat)) {
+                renderPlantUMLAsAscii(plantUmlSource);
+            } else {
+                System.err.println("Invalid render format. Use 'png' or 'ascii'.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        List<ClassRelation> relations = plantUML.getRelations();
-//        for (ClassRelation classRelation: relations) {
-//            System.out.println(classRelation.toString());
-//        }
-        System.out.println(plantUML.toString());
+    }
+
+    private static void renderPlantUMLAsPNG(String plantUmlSource) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        SourceStringReader reader = new SourceStringReader(plantUmlSource);
+        reader.outputImage(outputStream);
+
+        // Implement your logic for displaying or saving the PNG image
+        // For simplicity, let's just print the image bytes to the console
+        System.out.println(outputStream.toString());
+    }
+
+    private static void renderPlantUMLAsAscii(String plantUmlSource) {
+        // Render PlantUML as ASCII art
+        System.out.println("Rendering PlantUML as ASCII:\n" + plantUmlSource);
     }
 }
---
+++ JavaVisualizer/src/main/java/org/example/PlantUMLParser/Controller/XsdConverterVisitor.java
@@ -0,0 +1,136 @@
+package org.example.PlantUMLParser.Controller;
+
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Set;
+
+public class XsdConverterVisitor extends VoidVisitorAdapter<Set<String>> {
+
+    private static final String XML_NAMESPACE = "http://www.w3.org/2001/XMLSchema";
+    private static final String TARGET_NAMESPACE = "/jaxb/gen";
+
+    private BufferedWriter writer;
+
+    public XsdConverterVisitor(String outputPath) {
+        try {
+            this.writer = new BufferedWriter(new FileWriter(outputPath));
+        } catch (IOException e) {
+            throw new RuntimeException("Error creating XSD file: " + e.getMessage(), e);
+        }
+    }
+
+    public void closeWriter() {
+        try {
+            if (writer != null) {
+                writer.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing XSD file: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void visit(ClassOrInterfaceDeclaration classOrInterface, Set<String> projectClasses) {
+        writeLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        writeLine("<schema xmlns=\"" + XML_NAMESPACE + "\" targetNamespace=\"" + TARGET_NAMESPACE +
+                "\" xmlns:userns=\"" + TARGET_NAMESPACE + "\" elementFormDefault=\"qualified\">");
+
+        String className = classOrInterface.getNameAsString();
+        writeLine("  <element name=\"" + className + "\" type=\"userns:" + className + "\"></element>");
+
+        writeLine("  <complexType name=\"userns:" + className + "\">");
+        writeLine("    <sequence>");
+
+        classOrInterface.getFields().forEach((FieldDeclaration field) -> {
+            if (isPrimitiveOrDefined(field, projectClasses)) {
+                writeLine("      <element name=\"" + field.getVariables().get(0).getNameAsString() +
+                        "\" type=\"" + getXmlType(field.getElementType(), projectClasses) + "\"/>");
+            }
+        });
+
+        classOrInterface.getMethods().forEach((MethodDeclaration method) -> {
+            writeLine("      <element name=\"" + method.getNameAsString() +
+                    "\" type=\"" + getXmlType(method.getType(), projectClasses) + "\"/>");
+        });
+
+        writeLine("    </sequence>");
+        writeLine("  </complexType>");
+        writeLine("</schema>");
+
+        super.visit(classOrInterface, projectClasses);
+    }
+
+    private void writeLine(String line) {
+        try {
+            writer.write(line);
+            writer.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing to XSD file: " + e.getMessage(), e);
+        }
+    }
+
+    private boolean isPrimitive(String typeName) {
+        // Add mappings for other primitive types as needed
+        return typeName.equals("int") || typeName.equals("double") ||
+                typeName.equals("float") || typeName.equals("boolean") ||
+                typeName.equals("char") || typeName.equals("byte") ||
+                typeName.equals("short") || typeName.equals("long") ||
+                typeName.equals("String") || typeName.equals("Date");
+    }
+
+    private String getXmlType(Type type, Set<String> projectClasses) {
+        // Convert Java type to XML Schema type
+        if (type.isPrimitiveType()) {
+            return getXmlPrimitiveType(type.asString());
+        } else if (type.isClassOrInterfaceType()) {
+            ClassOrInterfaceType classType = type.asClassOrInterfaceType();
+            String typeName = classType.getNameAsString();
+
+            // Check if it's a known project class or a generic type
+            if (projectClasses.contains(typeName) || isGenericType(typeName)) {
+                return "tns:" + typeName; // Assuming tns is your custom namespace prefix
+            }
+        }
+
+        return "xs:string"; // Default to xs:string for other cases
+    }
+
+    private String getXmlPrimitiveType(String typeName) {
+        // Convert Java primitive type to XML Schema type
+        // ... (Add mappings for other primitive types as needed)
+        return switch (typeName) {
+            case "int" -> "xs:int";
+            case "double" -> "xs:double";
+            case "float" -> "xs:float";
+            case "boolean" -> "xs:boolean";
+            case "char" -> "xs:string"; // XML Schema does not have a specific type for char
+            case "byte" -> "xs:byte";
+            case "short" -> "xs:short";
+            case "long" -> "xs:long";
+            case "Date" -> "xs:dateTime"; // Assuming you want to map Java Date to xs:dateTime
+            default -> "xs:string";
+        };
+    }
+
+    private boolean isGenericType(String typeName) {
+        // Check if the type is a generic type
+        return typeName.contains("<") && typeName.endsWith(">");
+    }
+
+    private boolean isPrimitiveOrDefined(FieldDeclaration field, Set<String> projectClasses) {
+        VariableDeclarator variable = field.getVariables().get(0);
+        String typeName = variable.getTypeAsString();
+
+        // Check if the type is primitive, defined in the project, or a generic type
+        return isPrimitive(typeName) || projectClasses.contains(typeName) || isGenericType(typeName);
+    }
+}
